41bdd2a9b1257f38e598a66a8a1d0745ababddd1
Ed1kson <oplakanskiytitan@gmail.com>
2026-02-10 12:21:38 +0100

    added bye world

--- src/App.java
+++ src/App.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("bye world! ");
     }
 }
 
