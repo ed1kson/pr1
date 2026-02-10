8260b67ff491fa931225ed354908664e62392c61
Ed1kson <oplakanskiytitan@gmail.com>
2026-02-10 23:06:56 +0100

    added .gitignore and modified App.java

--- src/App.java
+++ src/App.java
@@ -4,6 +4,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("bye world! ");
+    
+        System.out.println("hello bitches");
     }
 }
 
